2b548054d5dfd18568ae8db5bcc5b39eec97bc6a
syed-abdul <jacksyed123@gmail.com>
2020-07-20 09:30:22 -0700

    rahman code added

--- src/org/read/SimpleJava.java
+++ src/org/read/SimpleJava.java
@@ -1,5 +1,14 @@
 package org.read;
 
 public class SimpleJava {
+	
+	public void ifscCode() {
+		System.out.println("sbin0015703");
+	
+	}
+	public static void main(String[] args) {
+		SimpleJava si=new SimpleJava();
+		si.ifscCode();
+	}
 
 }
